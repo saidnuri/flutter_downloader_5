c91e79762b734d5f6888d54f07529703736527d7
saidnuri <snur943@gmail.com>
2022-03-19 16:42:23 +0300

    master

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:flutter_downloader_5/dialog.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 import 'package:downloads_path_provider_28/downloads_path_provider_28.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_downloader_5/model.dart';
@@ -9,16 +11,14 @@ import 'package:http/http.dart' as http;
 import 'package:just_audio/just_audio.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DemoApp(),
-    );
-  }
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+  runApp(MaterialApp(
+    home: DemoApp(),
+  ));
 }
 
 class DemoApp extends StatefulWidget {
@@ -33,15 +33,23 @@ class _DemoAppState extends State<DemoApp> {
   List myList = [];
   List percent = [];
   List myListLink = [];
+  List isLoading = [];
+  List isPlaying = [];
+  late InterstitialAd _interstitialAd;
+
   AudioPlayer audioPlugin = AudioPlayer();
   late List videoResult1;
   final TextEditingController controller = TextEditingController();
+  // TODO: Add _bannerAd
+
   Future<void> callAPI(String query) async {
     try {
       basla = true;
       myList.clear();
       percent.clear();
       myListLink.clear();
+      isLoading.clear();
+      isPlaying.clear();
 
       final response = await http.get(
           Uri.parse('https://fastapiairtablem.herokuapp.com/snc/' + query));
@@ -49,20 +57,31 @@ class _DemoAppState extends State<DemoApp> {
           .map((data) => Model.fromJson(data))
           .toList();
       myList = List.generate(videoResult1.length, (index) => false);
+      isPlaying = List.generate(videoResult1.length, (index) => false);
       myListLink = List.generate(videoResult1.length, (index) => "");
       percent = List.generate(videoResult1.length, (index) => 0.0);
+      isLoading = List.generate(videoResult1.length, (index) => false);
       basla = false;
+      // ignore: empty_catches
     } catch (e) {}
     setState(() {});
   }
 
+  final BannerAd myBanner = BannerAd(
+    adUnitId: 'ca-app-pub-2567472206716852/7517473309',
+    size: AdSize.banner,
+    request: const AdRequest(),
+    listener: const BannerAdListener(),
+  );
+
   @override
   void initState() {
     super.initState();
-    callAPI("");
+    callAPI("fecr suresi");
+    myBanner.load();
   }
 
-  void _indir(videoTitle, index, id) async {
+  _indir(videoTitle, index, link) async {
     Map<Permission, PermissionStatus> statuses = await [
       Permission.storage,
       //add more permission to request here.
@@ -77,20 +96,17 @@ class _DemoAppState extends State<DemoApp> {
         //output:  /storage/emulated/0/Download/banner.png
 
         try {
-          var link = await Dio()
-              .get('https://fastapiairtablem.herokuapp.com/' + id.toString());
-
           await Dio().download(link.toString(), savePath,
               onReceiveProgress: (received, total) {
             if (total != -1) {
               setState(() {
                 percent[index] = received / total;
               });
-              print((received / total * 100));
+
               //you can build progressbar feature too
             }
           });
-          print("File is saved to download folder.");
+          return link;
         } on DioError catch (e) {
           print(e.message);
         }
@@ -102,54 +118,68 @@ class _DemoAppState extends State<DemoApp> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue[100],
-      appBar: AppBar(
-        title: Row(
-          children: <Widget>[
-            Expanded(
-              child: TextField(
-                controller: controller,
-                onSubmitted: (value) {
-                  setState(() {});
-                  callAPI(value);
-                },
-                style: const TextStyle(fontSize: 22.0, color: Colors.white),
-                decoration: InputDecoration(
-                    prefixIcon: IconButton(
-                        onPressed: () {
-                          controller.clear();
-                        },
+    return WillPopScope(
+      onWillPop: () => showExitPopup(context),
+      child: Scaffold(
+        backgroundColor: Colors.blue[100],
+        appBar: AppBar(
+          title: Row(
+            children: <Widget>[
+              Expanded(
+                child: TextField(
+                  controller: controller,
+                  onSubmitted: (value) {
+                    setState(() {});
+                    callAPI(value);
+                  },
+                  style: const TextStyle(fontSize: 22.0, color: Colors.white),
+                  decoration: InputDecoration(
+                      prefixIcon: IconButton(
+                          onPressed: () {
+                            controller.clear();
+                          },
+                          icon: const Icon(
+                            Icons.clear,
+                            color: Colors.white,
+                          )),
+                      suffixIcon: IconButton(
                         icon: const Icon(
-                          Icons.clear,
+                          Icons.search,
                           color: Colors.white,
-                        )),
-                    suffixIcon: IconButton(
-                      icon: const Icon(
-                        Icons.search,
-                        color: Colors.white,
+                        ),
+                        onPressed: () {
+                          callAPI(controller.text);
+                        },
                       ),
-                      onPressed: () {
-                        callAPI(controller.text);
-                      },
-                    ),
-                    hintText: "Search...",
-                    hintStyle: const TextStyle(color: Colors.white)),
+                      hintText: "Search...",
+                      hintStyle: const TextStyle(color: Colors.white)),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
+        body: basla
+            ? const Center(
+                child: CircularProgressIndicator(
+                color: Colors.blue,
+              ))
+            : Column(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: AdWidget(ad: myBanner),
+                  ),
+                  Expanded(
+                    flex: 9,
+                    child: ListView.builder(
+                        itemCount: videoResult1.length,
+                        itemBuilder: (context, index) {
+                          return listItem(videoResult1[index], index);
+                        }),
+                  ),
+                ],
+              ),
       ),
-      body: basla
-          ? const Center(
-              child: CircularProgressIndicator(
-              color: Colors.blue,
-            ))
-          : ListView.builder(
-              itemCount: videoResult1.length,
-              itemBuilder: (context, index) {
-                return listItem(videoResult1[index], index);
-              }),
     );
   }
 
@@ -157,68 +187,106 @@ class _DemoAppState extends State<DemoApp> {
     Model video,
     int index,
   ) {
-    var link;
     return Card(
       child: Container(
         margin: const EdgeInsets.symmetric(vertical: 7.0),
-        child: Column(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ListTile(
-              leading: Image.network(
-                video.thumbnails!.first.url.toString(),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child:
+                        Image.network(video.thumbnails!.first.url.toString()),
+                  ),
+                  Text("Süre: " + video.duration.toString(),
+                      style: const TextStyle(fontWeight: FontWeight.bold))
+                ],
               ),
-              title: Text(video.title.toString()),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    _indir(video.title, index, video.id);
-                  },
-                  child: Row(
-                    children: const [
-                      Text(
-                        "Download",
-                        style: TextStyle(color: Colors.blue),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(video.title.toString()),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      TextButton(
+                          onPressed: () async {
+                            isLoading[index] = true;
+                            setState(() {});
+                            myListLink[index] == ""
+                                ? myListLink[index] = await Dio().get(
+                                    'https://fastapiairtablem.herokuapp.com/' +
+                                        video.id.toString())
+                                : myListLink[index] = myListLink[index];
+                            setState(() {});
+                            isLoading[index] = false;
+                            _indir(video.title, index, myListLink[index]);
+                          },
+                          child: isLoading[index]
+                              ? const CircularProgressIndicator()
+                              : Row(
+                                  children: const [
+                                    Text(
+                                      "Download",
+                                      style: TextStyle(color: Colors.blue),
+                                    ),
+                                    Icon(
+                                      Icons.download,
+                                      size: 20,
+                                      color: Colors.blue,
+                                    )
+                                  ],
+                                )),
+                      TextButton(
+                        onPressed: () async {
+                          isPlaying[index] = true;
+                          setState(() {});
+
+                          myListLink[index] == ""
+                              ? myListLink[index] = await Dio().get(
+                                  'https://fastapiairtablem.herokuapp.com/' +
+                                      video.id.toString())
+                              : myListLink[index];
+                          isPlaying[index] = false;
+                          audioPlugin.setUrl(myListLink[index].toString());
+                          myList[index]
+                              ? audioPlugin.pause()
+                              : audioPlugin.play();
+                          setState(() {
+                            myList[index] = !myList[index];
+                          });
+                        },
+                        child: isPlaying[index]
+                            ? CircularProgressIndicator()
+                            : Row(children: [
+                                Text(
+                                  myList[index] ? "Pause" : "Play",
+                                  style: const TextStyle(color: Colors.blue),
+                                ),
+                                Icon(
+                                  myList[index]
+                                      ? Icons.pause
+                                      : Icons.play_arrow,
+                                  size: 20,
+                                  color: Colors.blue,
+                                ),
+                              ]),
                       ),
-                      Icon(
-                        Icons.download,
-                        size: 30,
-                        color: Colors.blue,
-                      )
                     ],
                   ),
-                ),
-                TextButton(
-                  onPressed: () async {
-                    myListLink[index] == ""
-                        ? myListLink[index] = await Dio().get(
-                            'https://fastapiairtablem.herokuapp.com/' +
-                                video.id.toString())
-                        : myListLink[index];
-
-                    audioPlugin.setUrl(myListLink[index].toString());
-                    myList[index] ? audioPlugin.pause() : audioPlugin.play();
-                    setState(() {
-                      myList[index] = !myList[index];
-                    });
-                  },
-                  child: Row(children: [
-                    Text(
-                      myList[index] ? "Pause" : "Play",
-                      style: const TextStyle(color: Colors.blue),
-                    ),
-                    Icon(
-                      myList[index] ? Icons.pause : Icons.play_arrow,
-                      size: 30,
-                      color: Colors.blue,
-                    ),
-                  ]),
-                ),
-              ],
+                  indicator(percent[index], 100)
+                ],
+              ),
             ),
-            indicator(percent[index], 200)
           ],
         ),
       ),
@@ -227,14 +295,12 @@ class _DemoAppState extends State<DemoApp> {
 }
 
 Widget indicator(double perc, double width) {
-  return Center(
-    child: LinearPercentIndicator(
-      width: width,
-      lineHeight: 20.0,
-      center: Text((perc * 100).toStringAsFixed(0) + "%"),
-      percent: perc,
-      backgroundColor: Colors.grey,
-      progressColor: Colors.blue,
-    ),
+  return LinearPercentIndicator(
+    width: width,
+    lineHeight: 20.0,
+    center: Text((perc * 100).toStringAsFixed(0) + "%"),
+    percent: perc,
+    backgroundColor: Colors.grey,
+    progressColor: Colors.blue,
   );
 }
